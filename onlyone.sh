d731506e07ed1831f002ba841ff9aae4c27db1e4
anyung533 <860300966@qq.com>
2021-08-16 00:18:01 +0800

    豆豆

---
+++ onlyone.sh
@@ -0,0 +1,24 @@
+cd /ql/scripts && npm install got
+cd /ql/scripts && npm install tough-cookie
+cd /ql/scripts && npm install axios date-fns
+cd /ql/scripts && npm install crypto-js
+cd /ql/scripts && npm install download
+cd /ql/scripts && npm install -g typescript
+cd /ql/scripts && npm i -S png-js
+cd /ql/scripts && npm install fs
+
+
+
+cd /ql/scripts && apk add libc6-compat
+cd /ql/scripts && apk add nodejs-current
+
+
+cd /ql/scripts && pip3 install requests
+
+
+cd /ql/scripts && apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
+
+
+cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && npm i && npm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
+
+cp -rf /ql/repo/anyung533_special/JS_USER_AGENTS.js /ql/scripts/      
--- yilai.sh
+++ yilai.sh
@@ -5,28 +5,9 @@ cp -rf /ql/repo/anyung533_special/utils/USER_AGENTS.js /ql/scripts/
 cp -rf /ql/repo/anyung533_special/utils/TS_USER_AGENTS.js /ql/scripts/
 cp -rf /ql/repo/anyung533_special/utils/TS_USER_AGENTS.ts /ql/scripts/
 cp -rf /ql/repo/anyung533_special/utils/JD_DailyBonus.js /ql/scripts/
-
-cd /ql/scripts && npm install got
-cd /ql/scripts && npm install tough-cookie
-cd /ql/scripts && npm install axios date-fns
-cd /ql/scripts && npm install crypto-js
-cd /ql/scripts && npm install download
-cd /ql/scripts && npm install -g typescript
-cd /ql/scripts && npm i -S png-js
-cd /ql/scripts && npm install fs
-
-
-
-cd /ql/scripts && apk add libc6-compat
-cd /ql/scripts && apk add nodejs-current
-
-
-cd /ql/scripts && pip3 install requests
-
-
-cd /ql/scripts && apk add python3 zlib-dev gcc jpeg-dev python3-dev musl-dev freetype-dev
-
-
-cd /ql/scripts/ && apk add --no-cache build-base g++ cairo-dev pango-dev giflib-dev && npm i && npm i -S ts-node typescript @types/node date-fns axios png-js canvas --build-from-source
-
-cp -rf /ql/repo/anyung533_special/JS_USER_AGENTS.js /ql/scripts/      
+cp -rf /ql/repo/anyung533_special/utils/jdPlantBeanShareCodes.js /ql/scripts/
+cp -rf /ql/repo/anyung533_special/utils/jdPetShareCodes.js /ql/scripts/
+cp -rf /ql/repo/anyung533_special/utils/jdJxncShareCodes.js /ql/scripts/
+cp -rf /ql/repo/anyung533_special/utils/jdFruitShareCodes.js /ql/scripts/
+cp -rf /ql/repo/anyung533_special/utils/jdFactoryShareCodes.js /ql/scripts/
+cp -rf /ql/repo/anyung533_special/utils/jdDreamFactoryShareCodes.js /ql/scripts/
