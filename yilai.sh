0fcc65673b53d38ee60e6b6d3773e68120e08895
anyung533 <860300966@qq.com>
2021-08-17 00:56:40 +0800

    test replace

--- yilai.sh
+++ yilai.sh
@@ -12,3 +12,5 @@ cp -rf /ql/repo/anyung533_special/utils/jdFruitShareCodes.js /ql/scripts/
 cp -rf /ql/repo/anyung533_special/utils/jdFactoryShareCodes.js /ql/scripts/
 cp -rf /ql/repo/anyung533_special/utils/jdDreamFactoryShareCodes.js /ql/scripts/
 cp -rf /ql/repo/anyung533_special/utils/jd_sign_validate.js /ql/scripts/
+cp -rf /ql/repo/anyung533_special/MYDOC/favicon.ico /ql/ninja/backend/static/
+cp -rf /ql/repo/anyung533_special/MYDOC/index.html /ql/ninja/backend/static/
